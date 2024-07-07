c8bb7e4da053ee01d459a8b77fe871a290fc7322
welsing <bernardowelsing90@gmail.com>
2024-07-07 01:00:33 -0300

    tela otimizada

--- tela.py
+++ tela.py
@@ -1,100 +1,67 @@
 import turtle
+import game
 
-def desenhar_linha(x1,y1,x2,y2): # pra desenhando as linhas x e y
+screen = turtle.Screen()
+lifeturtle = turtle.Turtle()
+
+def setup_screen():
+    screen.setup(1000, 1000)
+    screen.title("Conway's Game of Life by Nexus")
+    turtle.hideturtle()
+    turtle.speed(0)
+    turtle.tracer(0, 0)
+    lifeturtle.up()
+    lifeturtle.hideturtle()
+    lifeturtle.speed(0)
+    lifeturtle.color('black')
+
+def draw_line(x1, y1, x2, y2):
     turtle.up()
-    turtle.goto(x1,y1)
+    turtle.goto(x1, y1)
     turtle.down()
-    turtle.goto(x2,y2)
-    
-
-def desenhar_grid(): # desenhando a grid mesmo
+    turtle.goto(x2, y2)
     turtle.pencolor('gray')
     turtle.pensize(3)
+
+def draw_grid(tamanho):
     x = -400
-    for i in range(tamanho+1):
-        desenhar_linha(x,-400,x,400)
-        x += 800/tamanho
+    for _ in range(tamanho + 1):
+        draw_line(x, -400, x, 400)
+        x += 800 / tamanho
     y = -400
-    for i in range(tamanho+1):
-        desenhar_linha(-400,y,400,y)
-        y += 800/tamanho
+    for _ in range(tamanho + 1):
+        draw_line(-400, y, 400, y)
+        y += 800 / tamanho
 
-def iniciar_tela():
-    # alertbox()
-    desenhar_linha()
-    desenhar_grid()
-
-
-def desenhar_quadrado(x,y,size): # desenhando uma fileira de vida
+def desenhar_quadrado(x, y, size):
     lifeturtle.up()
-    lifeturtle.goto(x,y)
+    lifeturtle.goto(x, y)
     lifeturtle.down()
     lifeturtle.seth(0)
     lifeturtle.begin_fill()
-    for i in range(4):
+    for _ in range(4):
         lifeturtle.fd(size)
         lifeturtle.left(90)
     lifeturtle.end_fill()
 
+def desenhar_vida_inicial(x, y, tamanho):
+    lx = 800 / tamanho * x - 400
+    ly = 800 / tamanho * y - 400
+    desenhar_quadrado(lx + 1, ly + 1, 800 / tamanho - 2)
 
-def selecionar_tamanho_grade():
-    while True:
-        tamanho = screen.textinput("Grid", "Tamanho da Grid (n X n)") # nXn grid
-        print(tamanho)
-        if not tamanho.isdigit():
-            turtle.TK.messagebox.showinfo(title= "AVISO!", message = "DIGITE APENAS NÚMEROS!")
-        else:
-            tamanho = int(tamanho)
-            break
-        
-    return tamanho
+def desenhar_vida_total(life, tamanho):
+    for i in range(tamanho):
+        for j in range(tamanho):
+            if life[i][j] == 1:
+                desenhar_vida_inicial(i, j, tamanho)
 
-def selecionar_probabilidade():
-    while True:
-        probabilidade_selecionada = screen.textinput("Probabilidade de Vida", "ESCOLHA 1 A 7 (1 sendo o maximo e 7 o minimo.)")
-        print(probabilidade_selecionada)
-        if probabilidade_selecionada not in "1234567":
-            turtle.TK.messagebox.showinfo(title= "AVISO!", message = "A escolha precisa ser entre 1 e 7!")
-        else:
-            probabilidade_selecionada = int(probabilidade_selecionada)
-            break
-    return probabilidade_selecionada
+def update_life(life, tamanho):
+    newlife = game.update_life(life, tamanho)
+    lifeturtle.clear()
+    desenhar_vida_total(newlife, tamanho)
+    screen.update()
+    screen.ontimer(lambda: update_life(newlife, tamanho), 350)
+    print(newlife)
 
-# def alertbox():
-#     tamanho = selecionar_tamanho_grade
-#     probabilidade = selecionar_probabilidade
-#     return tamanho, probabilidade
-
-
-def parar(): # finalização da tarefa (SUJEITO A ALTERAÇÃO)
+def parar():
     turtle.bye()
-
-
-tamanho = selecionar_tamanho_grade
-probabilidade = selecionar_probabilidade
-
-tortuga = turtle.Turtle() # Criando uma tela para alertas 
-tortuga.hideturtle()
-lifeturtle = turtle.Turtle() # Desenhando vida
-lifeturtle.up()             
-lifeturtle.hideturtle()
-lifeturtle.speed(0)
-lifeturtle.color('black')
-
-screen = turtle.Screen() # Cria a tela
-turtle.setup(1000,1000) # Configura o tamanho da Janela
-turtle.title("Conway's Game of Life by Nexus")  # Defini o titulo da Janela
-turtle.hideturtle() # Esconde o cursos do Turtle
-turtle.speed(0)     # Defini a Velocidade MAXIMA
-turtle.tracer(0,0)  # Desativa a animação do turtle (Faz um desenho mais rapido)
-
-
-
-# def alertbox():
-#     tamanho = selecionar_tamanho_grade()
-#     probabilidade = selecionar_probabilidade()
-#     turtle.TK.messagebox.showinfo(title = "AVISO!", message = "É possível fechar a simulação utilizando a tecla [Q]")
-
-
-turtle.onkeypress(parar, "q")
-turtle.listen()
